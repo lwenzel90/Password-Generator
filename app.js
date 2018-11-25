4d7f219efa4a4a5d72552588c35a6e89307b4c0b
FirstBuiltPC <lwenzel90@gmail.com>
2018-11-25 14:35:29 -0500

    Options added to customize password created

--- app.js
+++ app.js
@@ -1,38 +1,59 @@
-document.getElementById('password').innerHTML = "Password: " + generatePassword(10);
-button = document.getElementById('createPassword');
+let button = document.getElementById('createPassword');
+let lowercaseCheckBox = document.getElementById("lowercase");
+let uppercaseCheckBox = document.getElementById("uppercase");
+let numberCheckBox = document.getElementById("numbers");
+let specialCheckBox = document.getElementById("special");
+
+// Show a password on load 
+window.onload = displayPassword;
 
 button.addEventListener('click', function(){
     document.getElementById('password').innerHTML = "Password: " + generatePassword(10);
+    console.log(lowercaseCheckBox.checked);
 });
 
-function generatePassword(len, optionsObj){
+function displayPassword(){
+    document.getElementById('password').innerHTML = "Password: " + generatePassword(10);
+}
+
+function generatePassword(len){
     let result = "";
     for(let i = 0; i < len; i++){
         let curr;
         // get a 0 1 or 2 then preform switch case 
-        // switch 0 lowercase, 1 upper, 2 numbers, 3 special values (!@#$%^&*)
+        // switch 0 uppercase, 1 lowercase, 2 numbers, 3 special values (!@#$%^&*)
         let caseNum = getRandomInt(0, 4);
         
         switch(caseNum) {
             case 0:
-                curr = getRandomInt(65, 90);
-                result += String.fromCharCode(curr);
-                break;
+                if(uppercaseCheckBox.checked === true){
+                    curr = getRandomInt(65, 90);
+                    result += String.fromCharCode(curr);
+                    break;
+                }
             case 1:
-                curr = getRandomInt(97, 122);
-                result += String.fromCharCode(curr);
-                break;
+                if(lowercaseCheckBox.checked === true){
+                    curr = getRandomInt(97, 122);
+                    result += String.fromCharCode(curr);
+                    break;
+                }
             case 2:
-                curr = getRandomInt(48, 57);
-                result += String.fromCharCode(curr);
-                break;
+                if(numberCheckBox.checked === true){
+                    curr = getRandomInt(48, 57);
+                    result += String.fromCharCode(curr);
+                    break;
+                }
             case 3:
-                curr = getRandomInt(33, 64);
-                result += String.fromCharCode(curr);
-                break;
+                if(specialCheckBox.checked === true){
+                    curr = getRandomInt(33, 64);
+                    result += String.fromCharCode(curr);
+                    break;
+                }
+            default: 
+                i--; // This makes sure that if no character is added then it will not count a letter
         }
     }
-    console.log(result);
+    console.log(result.length);
     return result;
 }
 
